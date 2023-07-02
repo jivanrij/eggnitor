4080e36939c909861d46b4da94edef080bb06dfc
Jonathan van Rij <jonathan@blijnder.nl>
2023-07-02 20:10:33 +0200

    removes older week options

--- app/Nova/Metrics/SoldNothingMetric.php
+++ app/Nova/Metrics/SoldNothingMetric.php
@@ -40,13 +40,7 @@ class SoldNothingMetric extends Value
     public function ranges()
     {
         return [
-            30 => __('30 Days'),
-            60 => __('60 Days'),
-            365 => __('365 Days'),
-            'TODAY' => __('Today'),
-            'MTD' => __('Month To Date'),
-            'QTD' => __('Quarter To Date'),
-            'YTD' => __('Year To Date'),
+            1 => __('This week'),
         ];
     }
 
